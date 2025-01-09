065233dde7b411e5f3837b15af298707b523bc4f
hank317 <zhenhu317@gmail.com>
2025-01-09 17:40:17 +0800

    uodate 1.0.0

--- conference_assistant/config.py
+++ conference_assistant/config.py
@@ -1,13 +1,13 @@
 redis_cluster_config = {
     'nodes':[
-        {'host': '172.28.5.64', 'port': 7001},
-        {'host': '172.28.5.64', 'port': 7002},
-        {'host': '172.28.5.64', 'port': 7003},
-        {'host': '172.28.5.64', 'port': 7004},
-        {'host': '172.28.5.64', 'port': 7005},
-        {'host': '172.28.5.64', 'port': 7006},
+        {'host': '127.0.0.1', 'port': 7001},
+        {'host': '127.0.0.1', 'port': 7002},
+        {'host': '127.0.0.1', 'port': 7003},
+        {'host': '127.0.0.1', 'port': 7004},
+        {'host': '127.0.0.1', 'port': 7005},
+        {'host': '127.0.0.1', 'port': 7006},
     ],
-    'password':"Hl95sms123!@#"
+    'password':"xxxxxxxxx"
 }
 
 redis_config = {
